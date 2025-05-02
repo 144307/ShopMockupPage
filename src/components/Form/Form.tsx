6526a1ce9029c8af55002c1bf920a07e563b4afd
144307 <67841399+144307@users.noreply.github.com>
2025-05-02 10:03:04 +0300

    refactor: improved password check messages

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,5 +1,4 @@
 import { useDispatch, useSelector } from "react-redux";
-// import { setOverlayClosed } from "../../features/ui/uiSlice";
 import "./Form.less";
 import { useEffect, useRef, useState } from "react";
 import { rootState } from "../../types";
@@ -11,9 +10,9 @@ interface Props {
 }
 
 interface Errors {
-  username?: string;
-  password?: string;
-  confirmPassword?: string;
+  username: string[];
+  password: string[];
+  confirmPassword: string[];
 }
 
 function Form({ mode: mode, onSubmit: onSubmit }: Props) {
@@ -28,7 +27,11 @@ function Form({ mode: mode, onSubmit: onSubmit }: Props) {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [passwordVisible, setPasswordVisible] = useState(false);
-  const [errors, setErrors] = useState<Errors>({});
+  const [errors, setErrors] = useState<Errors>({
+    username: [],
+    password: [],
+    confirmPassword: [],
+  });
   const passwordRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
@@ -37,12 +40,20 @@ function Form({ mode: mode, onSubmit: onSubmit }: Props) {
     }
   }, [ui.isOverlayOpened]);
 
+  useEffect(() => {
+    console.log("Errors", errors);
+  }, [errors]);
+
   function resetForm() {
     setUsername("");
     setPassword("");
     setConfirmPassword("");
     setPasswordVisible(false);
-    setErrors({});
+    setErrors({
+      username: [],
+      password: [],
+      confirmPassword: [],
+    });
   }
 
   function showPassword() {
@@ -57,33 +68,46 @@ function Form({ mode: mode, onSubmit: onSubmit }: Props) {
 
   function validate() {
     let valid = true;
-    const newErrors: Errors = {};
+    const newErrors: Errors = {
+      username: [],
+      password: [],
+      confirmPassword: [],
+    };
 
     if (!username.trim()) {
-      newErrors.username = "Username is required";
+      newErrors.username.push("Username is required");
       valid = false;
     }
     if (!password) {
-      newErrors.password = "Password is required";
+      newErrors.password.push("Password is required");
+      valid = false;
     }
     if (mode === "signup") {
       if (!confirmPassword) {
-        newErrors.confirmPassword = "Password is required";
+        newErrors.confirmPassword?.push("Password is required");
         valid = false;
       } else if (password !== confirmPassword) {
-        newErrors.confirmPassword = "Passwords do not match";
-        newErrors.password = "Passwords do not match";
+        newErrors.confirmPassword?.push("Passwords do not match");
+        newErrors.password?.push("Passwords do not match");
         valid = false;
-      } else if (
-        confirmPassword.length < 8 ||
-        !hasCapitalLetter.test(confirmPassword) ||
-        !hasNumber.test(confirmPassword)
-      ) {
-        newErrors.password =
-          "Password must be longer then 8 characters, have at lease one capital letter and one number";
-        newErrors.confirmPassword =
-          "Password must be longer then 8 characters, have at lease one capital letter and one number";
-        valid = false;
+      } else {
+        if (confirmPassword.length < 8) {
+          newErrors.password.push("Must be longer then 8 characters");
+          newErrors.confirmPassword.push("Must be longer then 8 characters");
+          valid = false;
+        }
+        if (!hasCapitalLetter.test(confirmPassword)) {
+          newErrors.password.push("Must have at lease one capital letter");
+          newErrors.confirmPassword.push(
+            "Must have at lease one capital letter"
+          );
+          valid = false;
+        }
+        if (!hasNumber.test(confirmPassword)) {
+          newErrors.password.push("Must have at lease one number");
+          newErrors.confirmPassword.push("Must have at lease one number");
+          valid = false;
+        }
       }
     }
 
@@ -109,7 +133,7 @@ function Form({ mode: mode, onSubmit: onSubmit }: Props) {
         <input
           autoFocus
           className={`form__input ${
-            errors.username ? "form__input_error" : ""
+            errors.username.length > 0 ? "form__input_error" : ""
           }`}
           id="username"
           type="text"
@@ -119,9 +143,11 @@ function Form({ mode: mode, onSubmit: onSubmit }: Props) {
           }}
         />
         <div
-          className={`form__error ${errors.username ? "form__error_show" : ""}`}
+          className={`form__error ${
+            errors.username.length > 0 ? "form__error_show" : ""
+          }`}
         >
-          {errors.username ? errors.username : ""}
+          {errors.username ? errors.username.map((e) => e) : ""}
         </div>
       </div>
       <div className="form__input-wrapper">
@@ -131,7 +157,7 @@ function Form({ mode: mode, onSubmit: onSubmit }: Props) {
         <div className="form__input-wrapper_2">
           <input
             className={`form__input form__input_left-part ${
-              errors.password ? "form__input_error" : ""
+              errors.password.length > 0 ? "form__input_error" : ""
             }`}
             ref={passwordRef}
             id="password"
@@ -145,7 +171,7 @@ function Form({ mode: mode, onSubmit: onSubmit }: Props) {
             className={`form__input_right-part ${
               passwordVisible ? "form__input_right-part_eye-closed" : ""
             } ${
-              errors.password
+              errors.password.length > 0
                 ? "form__input_right-part_error"
                 : "form__input_right-part_normal"
             }`}
@@ -154,9 +180,17 @@ function Form({ mode: mode, onSubmit: onSubmit }: Props) {
           />
         </div>
         <div
-          className={`form__error ${errors.password ? "form__error_show" : ""}`}
+          className={`form__error ${
+            errors.password.length > 0 ? "form__error_show" : ""
+          }`}
         >
-          {errors.password}
+          <ol>
+            {errors.password.map((e, i) => (
+              <li className="form__error-line" key={i}>
+                {e}
+              </li>
+            ))}
+          </ol>
         </div>
       </div>
       {mode === "signup" && (
@@ -166,7 +200,7 @@ function Form({ mode: mode, onSubmit: onSubmit }: Props) {
           </label>
           <input
             className={`form__input ${
-              errors.confirmPassword ? "form__input_error" : ""
+              errors.confirmPassword.length > 0 ? "form__input_error" : ""
             }`}
             id="confirm-password"
             type="password"
@@ -177,10 +211,16 @@ function Form({ mode: mode, onSubmit: onSubmit }: Props) {
           />
           <div
             className={`form__error ${
-              errors.confirmPassword ? "form__error_show" : ""
+              errors.confirmPassword.length > 0 ? "form__error_show" : ""
             }`}
           >
-            {errors.confirmPassword}
+            <ol>
+              {errors.password.map((e, i) => (
+                <li className="form__error-line" key={i}>
+                  {e}
+                </li>
+              ))}
+            </ol>
           </div>
         </div>
       )}
