cbca9792e2d8747fd156c45f730e8f0870621688
144307 <67841399+144307@users.noreply.github.com>
2025-06-16 14:01:13 +0300

    deploy: added basename in React Router

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ import "./styles/index.css";
 createRoot(document.getElementById("root")!).render(
   // <StrictMode>
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </Provider>
