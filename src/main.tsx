e79b4c3e8f29708a02a169f3863578f6fbd486ca
144307 <67841399+144307@users.noreply.github.com>
2025-08-21 18:14:36 +0300

    fix: uncommented strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,18 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { store } from "./store.ts";
 import { Provider } from "react-redux";
-// import { StrictMode } from "react";
+import { StrictMode } from "react";
 import { BrowserRouter } from "react-router";
 
 import "./styles/reset.css";
 import "./styles/index.css";
 
 createRoot(document.getElementById("root")!).render(
-  // <StrictMode>
-  <Provider store={store}>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <App />
-    </BrowserRouter>
-  </Provider>
-  // </StrictMode>
+  <StrictMode>
+    <Provider store={store}>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </StrictMode>
 );
